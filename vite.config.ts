df2b02ddafa7790c8727279300d4ca77a5060cd9
Aaryan Kalra <aryan.smart832@gmail.com>
2024-03-12 01:08:09 +0530

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import dotenv from "dotenv";
+dotenv.config();
+// https://vitejs.dev/config/
+// console.log(process.env.VITE_SERVER_URL, "process.env.VITE_SERVER_URL");
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true,
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: process.env.VITE_SERVER_URL,
+        secure: false,
+        changeOrigin: true,
+      },
+    },
+  },
+});
